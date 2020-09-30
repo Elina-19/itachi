0615c34e58711a46ba2cd4ef46057cdc754f731e
Zagidullina Elina <elina_3sha@mail.ru>
2020-09-30 21:46:17 +0300

    Program for string

--- Cos.java
+++
@@ -1,6 +0,0 @@
-public class Cos {
-	public static void main(String [] args) {
-		int n = Integer.parseInt(args[0]);
-		Double x = Double.parseDouble(args[1]);
-	}
-}
--- St.java
+++ St.java
@@ -1,24 +1,29 @@
 public class St {
 	public static void main(String[] args) {
-		String abc = "Java";
-		String abc2 = "a";
+		String abc = String.valueOf(args[0]);
+		String abc2 = String.valueOf(args[1]);
 		char a = abc2.charAt(0); //первый элемент подстроки
 		int l = abc2.length();
 		int len = abc.length();
 		char [] itachi = abc.toCharArray(); //строка в массив
 		int b = 0;
-		for (int i = 0; i<len; i++) {
-			if ((abc.charAt(i) == a) && (i+l<=abc.length())) { //если в строке находится 1-ый символ подстроки, то она обрезается и сравнивается с подстрокой
-				String abc3 = abc.substring(i,i+l);
-				if (abc3.compareTo(abc2) == 0) {
-					System.out.println(i);
-					b = 3;
-					break;
+		if ((args.length == 2) && (l<=len)) {
+			for (int i = 0; i<len; i++) {
+				if ((abc.charAt(i) == a) && (i+l<=abc.length())) { //если в строке находится 1-ый символ подстроки, то она обрезается и сравнивается с подстрокой
+					String abc3 = abc.substring(i,i+l);
+					if (abc3.compareTo(abc2) == 0) {
+						System.out.println(i);
+						b = 3;
+						break;
+					}
+				}
+				if ((b == 0) && (i == len-1)) {
+					System.out.println("-1");
 				}
 			}
+		}	
+		else {
+			System.out.println("Error");
 		}
-		if (b == 0) {
-			System.out.println("-1");
-		}
-	}
+	}	
 }
